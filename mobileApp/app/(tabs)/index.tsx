2e272d664b09485746d82d2a0332c1009361afb2
AroobMushtaq <aroobmushtaq786@gmail.com>
2025-06-24 11:37:29 +0500

    i learn about view,text,image,map,props

--- mobileApp/app/(tabs)/index.tsx
+++ mobileApp/app/(tabs)/index.tsx
@@ -1,12 +1,79 @@
-import React from 'react'
-import { View,Text } from 'react-native'
-import { red } from 'react-native-reanimated/lib/typescript/Colors'
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Image } from 'react-native'
+import Header from '@/components/Header'
+
 function Main() {
+  const [userName, setUsername] = useState("Aroob")
+
+  const handleNameChange = (name) => {
+    setUsername(name)
+  }
+  const data = [
+    { id: 1, name: 1 },
+    { id: 2, name: 2 },
+    { id: 3, name: 3 },
+    { id: 4, name: 4 },
+    { id: 5, name: 5 },
+    { id: 6, name: 6 },
+    { id: 7, name: 7 },
+    { id: 8, name: 8 }
+  ]
+
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#F5FCFF'}}>
-      <Text>Hello Mobile</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF' }}>
+      <Header title="this is props" />
+      <Image
+        style={styles.tinyLogo}
+        source={require('../../assets/images/react-logo.png')}
+      />
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={handleNameChange}
+        value={userName}
+        placeholder="Enter your name"
+        keyboardType="default"
+      />
+      <Text>Hello {userName}</Text>
+      {data.map(item => (
+        <Text key={item.id}>{item.name}</Text>
+      ))}
     </View>
+
+
   )
 }
 
 export default Main
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+  },
+  container: {
+    flex: 1,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  }
+})
