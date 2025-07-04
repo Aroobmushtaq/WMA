2e0bd7ed7777ecbd7736af7566e3ce73a00129a0
AroobMushtaq <aroobmushtaq786@gmail.com>
2025-07-04 13:16:16 +0500

    learn about parameters

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 3000
 app.get('/details',(req,res)=>{
     res.status(200).json([
@@ -7,6 +8,40 @@ app.get('/details',(req,res)=>{
         {name:"hadia",age:15}
     ])
 })
+// 1 client send data to server by query parameter 
+app.get('/query',(req,res)=>{
+    const name=req.query.name
+    if(!name){
+        console.log("name is not passed")
+        res.status(400).json({ error: "Name query parameter is required" })
+    }
+    console.log(name)
+    res.status(200).json([
+        {username:name}
+    ])
+})
+// 2 client send data to server by path parameter 
+app.get('/query/:id',(req,res)=>{
+    const id = req.params.id
+    if(!id){
+        console.log("id is not passed")
+        res.status(400).json({ error: "id  parameter is required" })
+    }
+    console.log(id)
+    res.status(200).json([
+        {userid:id}
+    ])
+})
+// 3 client send data to server by body  parameter 
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: "Username and password are required" });
+    }
+
+    res.status(200).json({ message: `Welcome, ${username}!` });
+});
 app.listen(port,()=>{
     console.log(`app is runing on port number ${port}`)
 })
